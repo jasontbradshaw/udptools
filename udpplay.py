4b47a57196bc5ae31c7a740c90129782a26b6e5a
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2010-12-06 01:00:27 -0600

    removed function tweaking from play method, moved former 'play' function to 'precise_play', rewrote 'play' to use far less CPU time.  it now uses 8-14% as apposed to 100%.

--- udpplay.py
+++ udpplay.py
@@ -1,12 +1,90 @@
 #!/usr/bin/env python
 
 import socket
-from binascii import a2b_base64 as b64decode
-from time import time, sleep
+import base64
+import time
 
 def play(dump_file, host, port):
     """
-    Plays a given dump file to the specified host and port.
+    Plays a given dump file to the specified host and port.  Doesn't play back
+    packets at the precise rate received, relying on the ability of any
+    receiving client to correctly buffer them and play them back at their
+    original rate in some other manner.  Doing this allows a far more efficient
+    use of CPU time than playing them back more precisely.
+    """
+
+    # TODO: fix the unknown corner case where there is a glitch very near the
+    # beginning of playback.  possibly related to the negative-time situation
+    # when sleeping.
+
+    # create the socket we'll send packets over
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # we make a function here that makes sending data quick and easy
+    addy = (host, port)
+    send_packet = lambda data: s.sendto(data, addy)
+
+    # used to store up packets before playing all at once
+    buflen = 100
+    buf = []
+
+    # read packets from the file and play them to the given address
+    with open(dump_file, 'r') as f:
+        next_play_time = None
+        last_play_time = None
+        first_packet_timestamp = None
+        for line in f:
+            # part before tab is time, part after is data followed by a newline
+            line_parts = line.split("\t")
+            packet_timestamp = float(line_parts[0])
+            packet_data = base64.b64decode(line_parts[1].rstrip()) # strip newline
+
+            # fill buffer, saving times of first and last packets
+            if len(buf) < buflen - 1:
+                # save the time of the first packet for later
+                if len(buf) == 0:
+                    first_packet_timestamp = packet_timestamp
+
+                buf.append(packet_data)
+                continue
+            else:
+                # append the last packet, the one read before buflen check failed
+                buf.append(packet_data)
+
+                # get first and last packet times and calculate total buffer time
+                buffer_time = packet_timestamp - first_packet_timestamp
+
+            # wait until the next buffer should be played. next_play_time is
+            # None before the first play.
+            if next_play_time is not None:
+                # if we took too much time parsing the last round's packets,
+                # play them immediately.
+                sleep_time = next_play_time - time.time()
+                if sleep_time > 0:
+                    time.sleep(sleep_time)
+
+            # send all packets in the buffer, marking time we started playing
+            # the buffer.
+            last_play_time = time.time()
+            map(send_packet, buf)
+
+            # set the next time we play as the number of seconds long the buffer
+            # was after the last time we played that buffer.
+            next_play_time = last_play_time + buffer_time
+
+            # reset buffer so we'll fill it again
+            buf = []
+
+        # wait until we should play what remains in the buffer
+        time.sleep(next_play_time - time.time())
+
+        # send what's left in the buffer
+        map(send_packet, buf)
+
+def precise_play(dump_file, host, port):
+    """
+    Plays a given dump file to the specified host and port, sending packets at
+    the precise rate they were received.  NOTE: Uses 100% CPU time!
     """
 
     # create the socket we'll send packets over
@@ -32,7 +110,7 @@ def play(dump_file, host, port):
             # part before tab is time, part after is data followed by a newline
             line_parts = line.split("\t")
             line_time = float(line_parts[0])
-            line_data = b64decode(line_parts[1].rstrip()) # strip newline
+            line_data = base64.b64decode(line_parts[1].rstrip()) # strip newline
 
             # TODO: find a way to reduce cpu usage while playing packets!
             # Ideas:
@@ -40,9 +118,9 @@ def play(dump_file, host, port):
             #  - remove rstrip and replace with a slicing off the final char
 
             # wait until we should play the next packet
-            loop_time = time()
+            loop_time = time.time()
             while loop_time - last_loop_time < line_time - last_line_time:
-                loop_time = time()
+                loop_time = time.time()
 
             # play the next packet
             s.sendto(line_data, addy)
