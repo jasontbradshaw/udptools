b6663844552a20655325ed378096251a95113198
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2011-02-18 18:46:38 -0600

    abstracted socket and file opening out of the *__loop functions to aid in testing

--- udptools.py
+++ udptools.py
@@ -36,10 +36,15 @@ class UDPPlay:
             raise AlreadyRunningError("Unable to start a new play process while"
                     " one is already running.  Stop playback first!")
 
-        args = (dump_file, host, port, begin_time, end_time)
-        self.__proc = mp.Process(target=self.__play_loop, args=args)
+        # create the socket we'll send packets over
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        self.__proc.start()
+        # open the file so we can pass it to the thread
+        with open(dump_file, 'r') as f:
+            args = (f, s, host, port, begin_time, end_time)
+            self.__proc = mp.Process(target=self.__play_loop, args=args)
+
+            self.__proc.start()
 
     def stop(self):
         """
@@ -55,7 +60,7 @@ class UDPPlay:
             self.__proc.join()
 
         # make sure the process was killed
-        assert not self.is_running()
+        assert not self.__proc.is_alive()
 
     def __parse_packet(self, packet):
         """
@@ -91,104 +96,103 @@ class UDPPlay:
 
         return timestamp, data
 
-    def __play_loop(self, dump_file, host, port, begin_time, end_time):
+    def __play_loop(self, dump_file, sock, host, port, begin_time, end_time):
         """
-        Plays a given dump file to the specified host and port.  Doesn't play
-        back packets at the precise rate received, relying on the ability of any
-        receiving client to correctly buffer them and play them back at their
-        original rate in some other manner.  Doing this allows a far more
+        Plays a given dump file object to the specified host and port.  Doesn't
+        play back packets at the precise rate received, relying on the ability
+        of any receiving client to correctly buffer them and play them back at
+        their original rate in some other manner.  Doing this allows a far more
         efficient use of CPU time than playing them back more precisely.
         """
 
-        # create the socket we'll send packets over
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # make sure we can read from the given file object
+        assert dump_file.mode.count("r") > 0
 
-        # we make a function here that makes sending data quick and easy
+        # make a function to make sending data quick and easy
         addy = (host, port)
-        send_packet = lambda data: s.sendto(data, addy)
+        send_packet = lambda data: sock.sendto(data, addy)
 
         # used to store up packets before playing all at once
         buflen = 100
         buf = []
 
+        # seek to the start position if a relevant begin_time was set
+        if begin_time > 0:
+            start_byte = self.find_timestamp_position(dump_file, begin_time)
+            dump_file.seek(start_byte)
+
         # read packets from the file and play them to the given address
-        with open(dump_file, 'r') as f:
-            # seek to the start position if a relevant begin_time was set
-            if begin_time > 0:
-                start_byte = self.find_timestamp_position(dump_file, begin_time)
-                f.seek(start_byte)
+        next_play_time = None
+        last_play_time = None
+        first_packet_timestamp = None
+        for line in dump_file:
+            # get the packet pieces so we can send them over the socket. if
+            # we fail to parse the packet, skip it.
+            try:
+                packet_timestamp, packet_data = self.__parse_packet(line)
+            except PacketParseError, ppe:
+                # TODO: log this instead
+                print ppe
+                continue
 
-            next_play_time = None
-            last_play_time = None
-            first_packet_timestamp = None
-            for line in f:
-                # get the packet pieces so we can send them over the socket. if
-                # we fail to parse the packet, skip it.
-                try:
-                    packet_timestamp, packet_data = self.__parse_packet(line)
-                except PacketParseError, ppe:
-                    # TODO: log this instead
-                    print ppe
-                    continue
+            # stop playback before the specified end time
+            if end_time is not None and packet_timestamp > end_time:
+                break
 
-                # stop playback before the specified end time
-                if end_time is not None and packet_timestamp > end_time:
-                    break
+            # add the packet to the buffer, since every packet must be added
+            # eventually
+            buf.append(packet_data)
 
-                # add the packet to the buffer, since every packet must be added
-                # eventually
-                buf.append(packet_data)
+            # save times of first and last packets
+            if len(buf) < buflen:
+                # save the time of the first packet for later
+                if len(buf) == 1:
+                    first_packet_timestamp = packet_timestamp
 
-                # save times of first and last packets
-                if len(buf) < buflen:
-                    # save the time of the first packet for later
-                    if len(buf) == 1:
-                        first_packet_timestamp = packet_timestamp
+                # keep filling until we're full
+                continue
+            else:
+                # get first and last packet times and calculate total buffer
+                # time.
+                buffer_time = packet_timestamp - first_packet_timestamp
 
-                    # keep filling until we're full
-                    continue
-                else:
-                    # get first and last packet times and calculate total buffer
-                    # time.
-                    buffer_time = packet_timestamp - first_packet_timestamp
-
-                # wait until the next buffer should be played. next_play_time is
-                # None before the first play.
-                if next_play_time is not None:
-                    # see how much time we should wait before playing the buffer
-                    sleep_time = next_play_time - time.time()
-
-                    # only sleep if we didn't take longer than the buflen to
-                    # parse this round's packets.
-                    if sleep_time > 0:
-                        time.sleep(sleep_time)
-
-                # send all packets in the buffer, marking time we started
-                # playing the buffer.
-                last_play_time = time.time()
-                map(send_packet, buf)
-
-                # set the next time we play as the number of seconds long the
-                # buffer was after the last time we played that buffer.
-                next_play_time = last_play_time + buffer_time
-
-                # reset buffer so we'll fill it again
-                buf = []
-
-            # wait until we should play what remains in the buffer.
-            # next_play_time can be 'None' if the buffer wasn't filled at least
-            # once while iterating over the file.
+            # wait until the next buffer should be played. next_play_time is
+            # None before the first play.
             if next_play_time is not None:
-                time.sleep(next_play_time - time.time())
+                # see how much time we should wait before playing the buffer
+                sleep_time = next_play_time - time.time()
 
-            # send what's left in the buffer
+                # only sleep if we didn't take longer than the buflen to
+                # parse this round's packets.
+                if sleep_time > 0:
+                    time.sleep(sleep_time)
+
+            # send all packets in the buffer, marking time we started
+            # playing the buffer.
+            last_play_time = time.time()
             map(send_packet, buf)
+
+            # set the next time we play as the number of seconds long the
+            # buffer was after the last time we played that buffer.
+            next_play_time = last_play_time + buffer_time
+
+            # reset buffer so we'll fill it again
+            buf = []
+
+        # wait until we should play what remains in the buffer.
+        # next_play_time can be 'None' if the buffer wasn't filled at least
+        # once while iterating over the file.
+        if next_play_time is not None:
+            time.sleep(next_play_time - time.time())
+
+        # send what's left in the buffer
+        map(send_packet, buf)
 
     def find_timestamp_position(self, dump_file, timestamp):
         """
         Finds the first position directly before the given position in the given
-        dump file.  Returns the file position in bytes such that the next read
-        from that position would be aligned with the beginning of the
+        dump file object.  Returns the file position in bytes such that the next
+        read from that position would be aligned with the beginning of the
         appropriate line, ie. the first line with a timestamp greater than or
         equal to the specified timestamp.  Returns 'None' if the requested
         timestamp could not be found in the file (was too far in the future, for
@@ -254,8 +258,7 @@ class UDPPlay:
                 return result
 
         # run our recursive function and return the seek position
-        with open(dump_file, 'r') as f:
-            return find_recursive(f, timestamp, 0, None)
+        return find_recursive(dump_file, timestamp, 0, None)
 
 class UDPDump:
     def __init__(self):
@@ -280,10 +283,14 @@ class UDPDump:
             raise AlreadyRunningError("Unable to start a new dump while one is "
                     "already running.  Stop the current dump first!")
 
-        args = (dump_file, host, port, max_packet_size)
-        self.__proc = mp.Process(target=self.__dump_loop, args=args)
+        # set up the socket we're capturing packets from
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        self.__proc.start()
+        with open(dump_file, 'w') as f:
+            args = (f, s, host, port, max_packet_size)
+            self.__proc = mp.Process(target=self.__dump_loop, args=args)
+
+            self.__proc.start()
 
     def stop(self):
         """
@@ -297,50 +304,51 @@ class UDPDump:
             self.__proc.join()
 
         # make sure the process was killed
-        assert not self.__proc.isalive()
+        assert not self.__proc.is_alive()
 
-    def __dump_loop(self, dump_file, host, port, max_packet_size):
+    def __dump_loop(self, dump_file, sock, host, port, max_packet_size):
         """
-        Dump UDP traffic to a file.
+        Dump UDP traffic to the given opened-for-writing file object.
         """
 
-        # set up the socket we're capturing packets from
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.bind((host, port))
+        # make sure we can write to the given file object
+        assert dump_file.mode.count("w") > 0 or dump_file.mode.count("a") > 0
+
+        # bind the given socket to the desired host and port
+        sock.bind((host, port))
 
         # the format of the lines written to the file, 10 place timestamp, tab,
         # data.
         file_format = "%.10f\t%s\n"
 
+        # time codes are relative to the first packet received, which has
+        # time 0.0. we set it after receive so any delay before traffic
+        # doesn't show up in the first timestamp.
+        first_packet_time = None
+
         # dump packets to the file
-        with open(dump_file, 'w') as f:
+        while 1:
+            # receive a packet and save the time we received it
+            raw_packet = sock.recv(max_packet_size)
+            packet_recv_time = time.time()
 
-            # time codes are relative to the first packet received, which has
-            # time 0.0. we set it after receive so any delay before traffic
-            # doesn't show up in the first timestamp.
-            first_packet_time = None
-            while 1:
-                # receive a packet and save the time we received it
-                raw_packet = s.recv(max_packet_size)
-                packet_recv_time = time.time()
+            # encode the raw binary packet data into a base64 string
+            packet_data = base64.b64encode(raw_packet)
 
-                # encode the raw binary packet data into a base64 string
-                packet_data = base64.b64encode(raw_packet)
+            # determine the time we'll write to file for this packet. this
+            # ensures that the first packet is always marked as 0.0.
+            packet_time = None
+            if first_packet_time is None:
+                # mark first received packet time
+                first_packet_time = packet_recv_time
+                packet_time = 0.0
+            else:
+                # calculate time since first packet
+                packet_time = packet_recv_time - first_packet_time
 
-                # determine the time we'll write to file for this packet. this
-                # ensures that the first packet is always marked as 0.0.
-                packet_time = None
-                if first_packet_time is None:
-                    # mark first received packet time
-                    first_packet_time = packet_recv_time
-                    packet_time = 0.0
-                else:
-                    # calculate time since first packet
-                    packet_time = packet_recv_time - first_packet_time
+            # make sure we've got a valid packet time
+            assert packet_time is not None
+            assert packet_time >= 0.0
 
-                # make sure we've got a valid packet time
-                assert packet_time is not None
-                assert packet_time >= 0.0
-
-                # write time elapsed from start plus data
-                f.write(file_format % (packet_time, packet_data))
+            # write time elapsed from start plus data
+            dump_file.write(file_format % (packet_time, packet_data))
